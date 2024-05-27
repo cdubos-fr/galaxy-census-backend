f98ce692034c5cb56d048b1c47b5074da4c5e6c6
cdubos-fr <contact@cdubos.fr>
2024-05-27 15:38:43 +0000

    feat: add nix support

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+    inputs = {
+        nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2405.630312.tar.gz";
+    };
+
+
+    # Flake outputs
+    outputs = { self, nixpkgs }:
+        let
+        # Systems supported
+        allSystems = [
+            "x86_64-linux" # 64-bit Intel/AMD Linux
+            "aarch64-linux" # 64-bit ARM Linux
+            "x86_64-darwin" # 64-bit Intel macOS
+            "aarch64-darwin" # 64-bit ARM macOS
+        ];
+
+        # Helper to provide system-specific attributes
+        forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+            pkgs = import nixpkgs { inherit system; };
+        });
+        in
+        {
+            # Development environment output
+            devShells = forAllSystems ({ pkgs }: {
+                default =
+                let
+                    python = pkgs.python310;
+                in
+                pkgs.mkShell {
+                    # The Nix packages provided in the environment
+                    packages = with pkgs; [
+                        # Python plus helper tools
+                        (python.withPackages (ps: with ps; [
+                            virtualenv
+                            pip
+                            tox
+                            pdm
+                        ]))
+                        just
+                        git
+                        pre-commit
+                        # ssh
+                    ];
+                    shellHook = ''
+                       just devenv && exec zsh
+                    '';
+                };
+            });
+        };
+}

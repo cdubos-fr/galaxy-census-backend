e5bcd42fed53f26253a8fcb1a233020a764bbe40
cdubos-fr <contact@cdubos.fr>
2024-06-02 19:09:39 +0000

    fix: direnv and flake

--- flake.nix
+++ flake.nix
@@ -1,24 +1,23 @@
 {
     inputs = {
-        nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2405.630312.tar.gz";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     };
 
-
     # Flake outputs
-    outputs = { self, nixpkgs }:
+    outputs = { self, nixpkgs, ... }:
         let
-        # Systems supported
-        allSystems = [
-            "x86_64-linux" # 64-bit Intel/AMD Linux
-            "aarch64-linux" # 64-bit ARM Linux
-            "x86_64-darwin" # 64-bit Intel macOS
-            "aarch64-darwin" # 64-bit ARM macOS
-        ];
+            # Systems supported
+            allSystems = [
+                "x86_64-linux" # 64-bit Intel/AMD Linux
+                "aarch64-linux" # 64-bit ARM Linux
+                "x86_64-darwin" # 64-bit Intel macOS
+                "aarch64-darwin" # 64-bit ARM macOS
+            ];
 
-        # Helper to provide system-specific attributes
-        forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-            pkgs = import nixpkgs { inherit system; };
-        });
+            # Helper to provide system-specific attributes
+            forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+                pkgs = import nixpkgs { inherit system; };
+            });
         in
         {
             # Development environment output
@@ -44,6 +43,7 @@
                     ];
                     shellHook = ''
                        just devenv
+                       source .venv/bin/activate
                     '';
                 };
             });
